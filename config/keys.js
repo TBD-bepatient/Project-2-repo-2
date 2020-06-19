48748139b319c6f2931772f8d76fb739f8bec225
Michael P Nance <michael.p.nance@gmail.com>
2020-06-18 21:08:41 -0400

    Reverted codebase to use localStrategy.

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    google: {
+        clientID: "776230935416-4tdtvahk1194vh9578kq0td9hk65m5ti.apps.googleusercontent.com",
+        clientSecret: "6A1TcApvD2dxiFmT0xcg6cc8"
+
+    }
+
+
+};
