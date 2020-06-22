3924001f6d5f4a9d96c21f164370c7d2c34a806f
SeanW <sean.walmer@outlook.com>
2020-06-22 17:51:38 -0400

    Added functionallity to buttons.

--- public/js/user.js
+++ public/js/user.js
@@ -1,13 +1,19 @@
 $(function () {
     $(".delete").on("click", function (event) {
         var id = $(this).data("id");
-
-        $.ajax("/api/games/" + id, {
-            type: "DELETE",
-        }).then(
-            function () {
-                location.reload();
-            }
-        );
+        console.log(id)
+        // $.ajax("/api/games/" + id, {
+        //     type: "DELETE",
+        // }).then(
+        //     function () {
+        //         location.reload();
+        //     }
+        // );
+    });
+    $(".search").on("click", function (event) {
+        console.log("searching games via api")
+    });
+    $(".send").on("click", function (event) {
+        console.log("sending data to database")
     });
 });
